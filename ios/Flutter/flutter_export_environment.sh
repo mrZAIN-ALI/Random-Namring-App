ea630c9e44830fb2de0e6d4e4acb28c67716b7fc
ZAIN ALI <97796695+mrZAIN-ALI@users.noreply.github.com>
2023-04-06 05:24:56 +0500

    correcting project

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\Users\zaina\Music\flutterSDK\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\App_Development\apks\FLutter_code_Lab\flutter_code_lab_1"
+export "FLUTTER_APPLICATION_PATH=C:\Users\zaina\Desktop\New folder\flutter_codelab_1"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=0.0.1"
+export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
